15a4c29e64b6bfb0140acb56149888e1f0a2c13e
Maxime Warnier <marmax@gmail.com>
2017-09-17 19:05:43 +0200

    add products

--- src/boutique/components/produit-detail.js
+++ src/boutique/components/produit-detail.js
@@ -1,36 +1,36 @@
-import React, {Component} from "react";
-import { connect } from "react-redux";
+import React, { Component } from "react"
+import { connect } from "react-redux"
 
-import produits from "../produits";
-import { Link } from "react-router";
+import produits from "../produits"
+import { Link } from "react-router"
 
-
-
-function createMarkup(markup) { return {__html: markup} }
+function createMarkup(markup) {
+  return { __html: markup }
+}
 
 if (!Array.prototype.find) {
-  Object.defineProperty(Array.prototype, 'find', {
+  Object.defineProperty(Array.prototype, "find", {
     value: function(predicate) {
-     // 1. Let O be ? ToObject(this value).
+      // 1. Let O be ? ToObject(this value).
       if (this == null) {
-        throw new TypeError('"this" is null or not defined');
+        throw new TypeError('"this" is null or not defined')
       }
 
-      var o = Object(this);
+      var o = Object(this)
 
       // 2. Let len be ? ToLength(? Get(O, "length")).
-      var len = o.length >>> 0;
+      var len = o.length >>> 0
 
       // 3. If IsCallable(predicate) is false, throw a TypeError exception.
-      if (typeof predicate !== 'function') {
-        throw new TypeError('predicate must be a function');
+      if (typeof predicate !== "function") {
+        throw new TypeError("predicate must be a function")
       }
 
       // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
-      var thisArg = arguments[1];
+      var thisArg = arguments[1]
 
       // 5. Let k be 0.
-      var k = 0;
+      var k = 0
 
       // 6. Repeat, while k < len
       while (k < len) {
@@ -38,94 +38,106 @@ if (!Array.prototype.find) {
         // b. Let kValue be ? Get(O, Pk).
         // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
         // d. If testResult is true, return kValue.
-        var kValue = o[k];
+        var kValue = o[k]
         if (predicate.call(thisArg, kValue, k, o)) {
-          return kValue;
+          return kValue
         }
         // e. Increase k by 1.
-        k++;
+        k++
       }
 
       // 7. Return undefined.
-      return undefined;
+      return undefined
     }
-  });
+  })
 }
 
 /**
  * Retoure le produit associé au lien
  */
-const getProduit = (lien) => produits.find(produit => produit.lien===lien)
+const getProduit = lien => produits.find(produit => produit.lien === lien)
 
-
-const ImagesProduit = ({imageIndex,images,nom,onImageSelect,lang}) => (
-
+const ImagesProduit = ({ imageIndex, images, nom, lang }) => (
   <div className="product-detail--image">
-          <div className="product-img-container">
-            <a href={"/images/boutique/"+images[imageIndex]}><img src={"/images/boutique/"+images[imageIndex]} alt={nom[lang]} draggable="false"/></a>
-          </div>
-
-          {images.length > 1 ? <div className="product-detail--images">
-            {images.map((image,i) => <img key={i}
-              src={"/images/boutique/"+image}
-              alt={nom[lang]}
-              onTouchStart={onImageSelect.bind(this,i)}
-              onMouseOver={onImageSelect.bind(this,i)}
-              draggable="false"
-              />)}
-          </div> : null }
-      </div>
-)
-
-
-const DetailsProduit = ({nom,prix,description,onAdd, lang}) => (
-   <div className="product-detail--container">
-      <h2 dangerouslySetInnerHTML={createMarkup(nom[lang])}></h2>
-      <div className="product-detail--prix" >{prix.toFixed(2)} €</div>
-      <div className="product-detail--description" dangerouslySetInnerHTML={createMarkup(description ? description[lang] : nom[lang])}></div>
-
-      <Link to="/cart" className="buy-button" onClick={onAdd}>{lang === "fr" ? "Ajouter au panier" :"Add to basket"}</Link>
-      <Link to="/">{lang === "fr" ? "Retour à la boutique" : "Back to the shop"}</Link>
+    <div className="product-img-container">
+      <a href={"/images/boutique/" + images[imageIndex]}>
+        <img src={"/images/boutique/" + images[imageIndex]} alt={nom[lang]} draggable="false" />
+      </a>
+    </div>
   </div>
 )
 
+const ListImages = ({ images, nom, onImageSelect, lang }) => (
+  <div style={{ display: "flex" }}>
+    {images.length > 1 ? (
+      <div className="product-detail--images">
+        {images.map((image, i) => (
+          <img
+            key={i}
+            src={"/images/boutique/" + image}
+            alt={nom[lang]}
+            onTouchStart={onImageSelect.bind(this, i)}
+            onMouseOver={onImageSelect.bind(this, i)}
+            draggable="false"
+          />
+        ))}
+      </div>
+    ) : null}
+  </div>
+)
+
+const DetailsProduit = ({ nom, prix, description, onAdd, lang }) => (
+  <div className="product-detail--container">
+    <h2 dangerouslySetInnerHTML={createMarkup(nom[lang])} />
+    <div className="product-detail--prix">{prix.toFixed(2)} €</div>
+    <div
+      className="product-detail--description"
+      dangerouslySetInnerHTML={createMarkup(description ? description[lang] : nom[lang])}
+    />
+
+    <Link to="/cart" className="buy-button" onClick={onAdd}>
+      {lang === "fr" ? "Ajouter au panier" : "Add to basket"}
+    </Link>
+  </div>
+)
 
 class Product extends Component {
-  constructor(props){
-    super(props);
+  constructor(props) {
+    super(props)
     this.state = {
-      imageIndex : 0
-    };
+      imageIndex: 0
+    }
   }
-  selectionImage(index){
+  selectionImage(index) {
     this.setState({
-      imageIndex : index
-    });
+      imageIndex: index
+    })
   }
-  render(){
-    const produit = getProduit(this.props.params.name);
+  render() {
+    const produit = getProduit(this.props.params.name)
     return (
       <div className="product-detail">
-        <ImagesProduit onImageSelect={this.selectionImage.bind(this)} imageIndex={this.state.imageIndex} {...produit} />
-        <DetailsProduit onAdd={this.props.onAdd.bind(this,produit)} {...produit} lang={this.props.lang} />
+        <div style={{ display: "flex" }}>
+          <ImagesProduit imageIndex={this.state.imageIndex} {...produit} />
+          <DetailsProduit onAdd={this.props.onAdd.bind(this, produit)} {...produit} lang={this.props.lang} />
+        </div>
+        <ListImages {...produit} onImageSelect={this.selectionImage.bind(this)} />
+        <Link to="/" style={{ textAlign: "right" }}>
+          {this.props.lang === "fr" ? "Retour à la boutique" : "Back to the shop"}
+        </Link>
       </div>
     )
   }
 }
 
-
-
 function mapDispatchToProps(dispatch) {
   return {
-    onAdd: (item) => dispatch({
-      type : "ADD",
-      item
-    })
-  };
+    onAdd: item =>
+      dispatch({
+        type: "ADD",
+        item
+      })
+  }
 }
 
-export default connect(
-  ({lang}) => ({lang}),
-  mapDispatchToProps
-)(Product);
-
+export default connect(({ lang }) => ({ lang }), mapDispatchToProps)(Product)
--- src/boutique/produits.js
+++ src/boutique/produits.js
@@ -1,207 +1,341 @@
-const produits = [{
-  nom: {
-    fr: "L'oracle des reflets",
-    en: "L'oracle des reflets"
-  },
-  lien: "deck",
-  cat: {
-    fr: "Les Jeux",
-    en: "The Decks"
-  },
-  prix: 35,
-  poids: 300,
-  images: ["jeu/boite.jpg","jeu/1.jpg","jeu/2.jpg","jeu/3.jpg","jeu/4.jpg","jeu/5.jpg","jeu/6.jpg","jeu/7.jpg","jeu/8.jpg"],
-  description: {
-    fr : `Un jeu de 57 cartes créé et illustré par Célia Melesville, accompagné d'un livret qui vous aidera dans vos débuts avec l'oracle.</br>
+const produits = [
+  {
+    nom: {
+      fr: "L'oracle des reflets",
+      en: "L'oracle des reflets"
+    },
+    lien: "deck",
+    cat: {
+      fr: "Les Jeux",
+      en: "The Decks"
+    },
+    prix: 35,
+    poids: 300,
+    images: [
+      "jeu/boite.jpg",
+      "jeu/1.jpg",
+      "jeu/2.jpg",
+      "jeu/3.jpg",
+      "jeu/4.jpg",
+      "jeu/5.jpg",
+      "jeu/6.jpg",
+      "jeu/7.jpg",
+      "jeu/8.jpg"
+    ],
+    description: {
+      fr: `Un jeu de 57 cartes créé et illustré par Célia Melesville, accompagné d'un livret qui vous aidera dans vos débuts avec l'oracle.</br>
     <p>Finitions : vernis mat , les cartes sont dorées sur la tranche. Boitier rigide.</p>`,
-    en : `A set of 57 cards created and illustrated by Celia Melesville, accompanied by a booklet that will help you get started with Oracle. </ Br>
+      en: `A set of 57 cards created and illustrated by Celia Melesville, accompanied by a booklet that will help you get started with Oracle. </ Br>
     <P> Finishes: matt varnish, the cards are golden on the edge. rigid case.`
-  }
-}, {
-  nom: {
-    fr: "L'oracle des reflets <small>[numéroté signé] + marque page</small>",
-    en: "L'oracle des reflets <small>[signed and numbered] + bookmark</small>"
+    }
   },
-  lien: "jeu-limite",
-  cat: {
-    fr: "Les Jeux",
-    en: "The Decks"
-  },
-  prix: 37,
-  poids: 300,
-  images: ["jeu/boite.jpg","jeu/1.jpg","jeu/2.jpg","jeu/3.jpg","jeu/4.jpg","jeu/5.jpg","jeu/6.jpg","jeu/7.jpg","jeu/8.jpg"],
-  description: {
-    fr : `Un jeu de 57 cartes créé et illustré par Célia Melesville, accompagné d'un livret qui vous aidera dans vos débuts avec l'oracle.
+  {
+    nom: {
+      fr: "L'oracle des reflets <small>[numéroté signé] + marque page</small>",
+      en: "L'oracle des reflets <small>[signed and numbered] + bookmark</small>"
+    },
+    lien: "jeu-limite",
+    cat: {
+      fr: "Les Jeux",
+      en: "The Decks"
+    },
+    prix: 37,
+    poids: 300,
+    images: [
+      "jeu/boite.jpg",
+      "jeu/1.jpg",
+      "jeu/2.jpg",
+      "jeu/3.jpg",
+      "jeu/4.jpg",
+      "jeu/5.jpg",
+      "jeu/6.jpg",
+      "jeu/7.jpg",
+      "jeu/8.jpg"
+    ],
+    description: {
+      fr: `Un jeu de 57 cartes créé et illustré par Célia Melesville, accompagné d'un livret qui vous aidera dans vos débuts avec l'oracle.
     <p>Finitions : vernis mat , les cartes sont dorées sur la tranche. Boitier rigide.</p>
     <p>Cette <strong> édition speciale contient un jeu numéroté et signé</strong> ainsi qu'un marque page</p>`,
-    en : `A set of 57 cards created and illustrated by Celia Melesville, accompanied by a booklet that will help you get started with Oracle. </ Br>
+      en: `A set of 57 cards created and illustrated by Celia Melesville, accompanied by a booklet that will help you get started with Oracle. </ Br>
     <p> Finishes: matt varnish, the cards are golden on the edge. rigid case.
     <p>This <strong> special edition includes a numbered and signed deck</strong> along with a bookmark</p>`
-  }
-},{
-  nom: {
-    fr: "Tanis Lenormand",
-    en: "Tanis Lenormand"
+    }
   },
-  lien: "tanis",
-  cat: {
-    fr: "Les Jeux",
-    en: "The Decks"
+  {
+    nom: {
+      fr: "Tanis Lenormand",
+      en: "Tanis Lenormand"
+    },
+    lien: "tanis",
+    cat: {
+      fr: "Les Jeux",
+      en: "The Decks"
+    },
+    prix: 32,
+    poids: 120,
+    images: ["tanis/tanis.png", "tanis/0.jpg", "tanis/1.jpg", "tanis/2.jpg"],
+    description: {
+      fr: `Ce jeu est constitué de 40 cartes et d'un petit livre pour vous aider dans l'interprétation de vos tirages`,
+      en: `This deck includes 40 cards and a book ( is bilingual so it is also in English) to help you interpret your spreads.`
+    }
   },
-  prix: 32,
-  poids: 120,
-  images: ["tanis/tanis.png"],
-  description: {
-    fr : `Ce jeu est constitué de 40 cartes et d'un petit livre pour vous aider dans l'interprétation de vos tirages`,
-    en : `This deck includes 40 cards and a book ( is bilingual so it is also in English) to help you interpret your spreads.`
-  }
-},{
-  nom: {
-    fr: "Marque Page",
-    en: "Bookmark"
+  {
+    nom: {
+      fr: "Runes",
+      en: "Runes"
+    },
+    lien: "runes",
+    cat: {
+      fr: "Runes",
+      en: "Runes"
+    },
+    prix: 32,
+    poids: 300,
+    images: [
+      "runes/0.jpg",
+      "runes/1.jpg",
+      "runes/2.jpg",
+      "runes/3.jpg",
+      "runes/4.jpg",
+      "runes/5.jpg",
+      "runes/6.jpg"
+    ],
+    description: {
+      fr: `...`,
+      en: `...`
+    }
   },
-  lien: "marque-page-oracle",
-  cat: {
-    fr: "Marque page",
-    en: "Bookmark"
-  },
-  prix: 2,
-  poids: 5,
-  images: ["marquepage/marque-page.jpg", "marquepage/marque-page-recto.jpg", "marquepage/marque-page-verso.jpg"],
-  description: {
-    fr : `Un marque page de qualité aux couleurs de l'oracle.
+  {
+    nom: {
+      fr: "Marque Page",
+      en: "Bookmark"
+    },
+    lien: "marque-page-oracle",
+    cat: {
+      fr: "Marque page",
+      en: "Bookmark"
+    },
+    prix: 2,
+    poids: 5,
+    images: [
+      "marquepage/marque-page.jpg",
+      "marquepage/marque-page-recto.jpg",
+      "marquepage/marque-page-verso.jpg"
+    ],
+    description: {
+      fr: `Un marque page de qualité aux couleurs de l'oracle.
     <p>Taille : 5.5cm*20cm</p>
     <p>Finition : vernis mat</p>`,
-    en : `Un marque page de qualité aux couleurs de l'oracle.
+      en: `A beautiful bookmark inspired by the Oracle des reflets Deck`
+    }
+  },
+  {
+    nom: {
+      fr: "Marque Page Tanis",
+      en: "Tanis's Bookmark"
+    },
+    lien: "marque-page-tanis",
+    cat: {
+      fr: "Marque page",
+      en: "Bookmark"
+    },
+    prix: 2,
+    poids: 5,
+    images: ["marquepage/tanis.jpg"],
+    description: {
+      fr: `Un marque page de qualité aux couleurs du Tanis lenormand.
     <p>Taille : 5.5cm*20cm</p>
-    <p>Finition : vernis mat</p>`
-  }
-}, {
-  nom: {
-    fr: "Pack 6 cartes postales + 1 marque page",
-    en: "6 cards + 1 bookmark"
+    <p>Finition : vernis mat</p>`,
+      en: `A beautiful bookmark inspired by the Tanis Lenormand Deck.
+    `
+    }
   },
-  lien: "pack6-marque-page",
-  cat: {
-    fr: "Les Packs",
-    en: "Packs"
+  {
+    nom: {
+      fr: "Pack 6 cartes postales + 1 marque page",
+      en: "6 cards + 1 bookmark"
+    },
+    lien: "pack6-marque-page",
+    cat: {
+      fr: "Les Packs",
+      en: "Packs"
+    },
+    prix: 15,
+    poids: 39,
+    images: [
+      "pack6/pack6.jpg",
+      "pack6/construction.jpg",
+      "pack6/ralentissement.jpg",
+      "pack6/renard.jpg",
+      "pack6/dualite.jpg",
+      "pack6/dispersion.jpg"
+    ],
+    description: {
+      fr: "Pack de 6 cartes postales accompagné un marque page",
+      en: "6 postal cards pack with a Bookmark "
+    }
   },
-  prix: 15,
-  poids: 39,
-  images: ["pack6/pack6.jpg", "pack6/construction.jpg", "pack6/ralentissement.jpg", "pack6/renard.jpg", "pack6/dualite.jpg",
-    "pack6/dispersion.jpg"
-  ],
-  description: {
-    fr : "Pack de 6 cartes postales accompagné un marque page",
-    en :"6 postal cards pack with a Bookmark "
-  }
-}, {
-  nom: {
-    fr : "Pack Saisons",
-    en : "Seasons Pack"
+  {
+    nom: {
+      fr: "Pack Saisons",
+      en: "Seasons Pack"
+    },
+    lien: "pack-saison",
+    cat: {
+      fr: "Les Packs",
+      en: "Packs"
+    },
+    prix: 10,
+    poids: 24,
+    images: [
+      "packsaison/saison.jpg",
+      "packsaison/automne.jpg",
+      "packsaison/ete.jpg",
+      "packsaison/hiver.jpg",
+      "packsaison/printemps.jpg"
+    ]
   },
-  lien: "pack-saison",
-  cat: {
-    fr: "Les Packs",
-    en: "Packs"
-  },
-  prix: 10,
-  poids: 24,
-  images: ["packsaison/saison.jpg",
-    "packsaison/automne.jpg", "packsaison/ete.jpg", "packsaison/hiver.jpg", "packsaison/printemps.jpg"
-  ]
-}, {
-  nom: {
-    fr : "Carte Postale - La Chouette",
-    en : "Post card - The Owl "
-  },
-  lien: "carte-chouette",
-  cat: {
-    fr: "Cartes Postales",
-    en: "Post cards"
-  },
-  prix: 2.50,
-  poids: 5,
-  images: ["/cartepostale/chouette.jpg"]
-}, {
-  nom: {
-    fr : "Carte Postale - La Construction",
-    en : "Post card - The Contruction"
-  },
-  lien: "carte-construction",
-  cat: {
-    fr: "Cartes Postales",
-    en: "Post cards"
-  },
-  prix: 2.50,
-  poids: 5,
-  images: ["/cartepostale/construction.jpg"]
-}, {
-  nom: {
-    fr : "Carte Postale - Dispersion",
-    en : "Post card - Dispersion"
-  },
-  lien: "carte-dispersion",
-  cat: {
-    fr: "Cartes Postales",
-    en: "Post cards"
-  },
-  prix: 2.50,
-  poids: 5,
-  images: ["/cartepostale/dispersion.jpg"]
-}, {
-  nom: {
-    fr : "Carte Postale - Dualité",
-    en :"Post card - Duality"
-  },
-  lien: "carte-dualite",
-  cat: {
-    fr: "Cartes Postales",
-    en: "Post cards"
-  },
-  prix: 2.50,
-  poids: 5,
-  images: ["/cartepostale/dualite.jpg"]
-}, {
-  nom: {
-    fr : "Carte Postale - Ralentissement",
-    en :"Post card - Slow-down"
-  },
-  lien: "carte-ralentissement",
-  cat: {
-    fr: "Cartes Postales",
-    en: "Post cards"
-  },
-  prix: 2.50,
-  poids: 5,
-  images: ["/cartepostale/ralentissement.jpg"]
-}, {
-  nom: {
-    fr : "Carte Postale - Renard",
-    en :"Post card - Fox"
-  },
-  lien: "carte-renard",
-  cat: {
-    fr: "Cartes Postales",
-    en: "Post cards"
-  },
-  prix: 2.50,
-  poids: 5,
-  images: ["/cartepostale/renard.jpg"]
-}, {
-  nom: {
-    fr : "Carte Postale - Hiver",
-    en :"Post card - Winter"
-  },
-  lien: "carte-hiver",
-  cat: {
-    fr: "Cartes Postales",
-    en: "Post cards"
-  },
-  prix: 2.50,
-  poids: 5,
-  images: ["/cartepostale/hiver.jpg"]
-}];
 
+  {
+    nom: {
+      fr: "Carte Postale - Coeur",
+      en: "Post card - Heart "
+    },
+    lien: "carte-coeur",
+    cat: {
+      fr: "Cartes Postales",
+      en: "Post cards"
+    },
+    prix: 2.5,
+    poids: 5,
+    images: ["/cartepostale/tanis_coeur.jpg"]
+  },
+  {
+    nom: {
+      fr: "Carte Postale - Renard",
+      en: "Post card - Tanis Fox "
+    },
+    lien: "carte-tanis-renard",
+    cat: {
+      fr: "Cartes Postales",
+      en: "Post cards"
+    },
+    prix: 2.5,
+    poids: 5,
+    images: ["/cartepostale/tanis_renard.jpg"]
+  },
+  {
+    nom: {
+      fr: "Carte Postale - Trèfle",
+      en: "Post card - Clover "
+    },
+    lien: "carte-trefle",
+    cat: {
+      fr: "Cartes Postales",
+      en: "Post cards"
+    },
+    prix: 2.5,
+    poids: 5,
+    images: ["/cartepostale/tanis_trefle.jpg"]
+  },
+  {
+    nom: {
+      fr: "Carte Postale - La Chouette",
+      en: "Post card - The Owl "
+    },
+    lien: "carte-chouette",
+    cat: {
+      fr: "Cartes Postales",
+      en: "Post cards"
+    },
+    prix: 2.5,
+    poids: 5,
+    images: ["/cartepostale/chouette.jpg"]
+  },
+  {
+    nom: {
+      fr: "Carte Postale - La Construction",
+      en: "Post card - The Contruction"
+    },
+    lien: "carte-construction",
+    cat: {
+      fr: "Cartes Postales",
+      en: "Post cards"
+    },
+    prix: 2.5,
+    poids: 5,
+    images: ["/cartepostale/construction.jpg"]
+  },
+  {
+    nom: {
+      fr: "Carte Postale - Dispersion",
+      en: "Post card - Dispersion"
+    },
+    lien: "carte-dispersion",
+    cat: {
+      fr: "Cartes Postales",
+      en: "Post cards"
+    },
+    prix: 2.5,
+    poids: 5,
+    images: ["/cartepostale/dispersion.jpg"]
+  },
+  {
+    nom: {
+      fr: "Carte Postale - Dualité",
+      en: "Post card - Duality"
+    },
+    lien: "carte-dualite",
+    cat: {
+      fr: "Cartes Postales",
+      en: "Post cards"
+    },
+    prix: 2.5,
+    poids: 5,
+    images: ["/cartepostale/dualite.jpg"]
+  },
+  {
+    nom: {
+      fr: "Carte Postale - Ralentissement",
+      en: "Post card - Slow-down"
+    },
+    lien: "carte-ralentissement",
+    cat: {
+      fr: "Cartes Postales",
+      en: "Post cards"
+    },
+    prix: 2.5,
+    poids: 5,
+    images: ["/cartepostale/ralentissement.jpg"]
+  },
+  {
+    nom: {
+      fr: "Carte Postale - Renard",
+      en: "Post card - Fox"
+    },
+    lien: "carte-renard",
+    cat: {
+      fr: "Cartes Postales",
+      en: "Post cards"
+    },
+    prix: 2.5,
+    poids: 5,
+    images: ["/cartepostale/renard.jpg"]
+  },
+  {
+    nom: {
+      fr: "Carte Postale - Hiver",
+      en: "Post card - Winter"
+    },
+    lien: "carte-hiver",
+    cat: {
+      fr: "Cartes Postales",
+      en: "Post cards"
+    },
+    prix: 2.5,
+    poids: 5,
+    images: ["/cartepostale/hiver.jpg"]
+  }
+]
 
-export default produits.map((produit, i) => ({...produit, id: i}))
+export default produits.map((produit, i) => ({ ...produit, id: i }))
