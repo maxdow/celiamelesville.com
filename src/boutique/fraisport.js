adbfa992d3ff86e3e9f4d54a7242c05180f6a15f
Maxime Warnier <marmax@gmail.com>
2018-06-04 15:35:03 +0200

    ajout frais de port multiples

--- src/boutique/fraisport.js
+++ src/boutique/fraisport.js
@@ -1,4 +1,4 @@
-const poidsEnvoie = [
+const portStandard = [
   {
     fr: 0.9,
     eu: 1.2,
@@ -37,29 +37,40 @@ const poidsEnvoie = [
 ]
 
 const limitePoids = [20, 50, 100, 250, 500, 1000, 2000]
+const limitePoidsLili = [500, 750, 1000, 2000]
 
-const portLili = {
-  g500: {
+const portLili = [
+  {
     fr: 6.5,
-    eu: 6.5,
-    ww: 6.5
+    eu: 12.5,
+    ww: 15
   },
-  g1000: {
+  {
+    fr: 6.5,
+    eu: 14,
+    ww: 17.5
+  },
+  {
     fr: 8,
-    eu: 6.5,
-    ww: 6.5
+    eu: 16.5,
+    ww: 18.5
+  },
+  {
+    fr: 9,
+    eu: 19.5,
+    ww: 27
   }
-}
+]
 
 export function calculFraisPort(poids, typePort, lilipresent) {
-  if (lilipresent) {
-    return poids > 500 ? portLili.g500[typePort] : portLili.g1000[typePort]
-  } else {
-    for (var i = 0; i < 7; i++) {
-      if (poids < limitePoids[i]) {
-        break
-      }
+  const typeLimite = lilipresent ? limitePoidsLili : limitePoids
+  const listePrix = lilipresent ? portLili : portStandard
+
+  for (var i = 0; i < typeLimite.length; i++) {
+    if (poids < typeLimite[i]) {
+      break
     }
-    return poidsEnvoie[i][typePort]
   }
+  return listePrix[i][typePort]
 }
+
